9592b335f134c6c4117555f88662cdded730b140
M. Daeva <embedmad@gmail.com>
2025-03-25 21:07:19 +0300

    added updateUserAssets

--- src/backend/controllers/api.ts
+++ src/backend/controllers/api.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import {
   getAverageEntryPrice as _getAverageEntryPrice,
   getProfit as _getProfit,
+  updateUserAssets as _updateUserAssets,
 } from "../middleware/api";
 
 export async function getAverageEntryPrice(req: Request, res: Response) {
@@ -39,5 +40,16 @@ export async function getProfit(req: Request, res: Response) {
   }
 
   const data = await _getProfit(address, from, to);
+  res.status(200).json(data);
+}
+
+export async function updateUserAssets(req: Request, res: Response) {
+  const address = req.body.address as string;
+
+  if (!address) {
+    res.status(400).json({ error: "Address parameter is required" });
+  }
+
+  const data = await _updateUserAssets(address);
   res.status(200).json(data);
 }
--- src/backend/routes/api.ts
+++ src/backend/routes/api.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { ROUTE } from "../constants";
-import { getAverageEntryPrice, getProfit } from "../controllers/api";
+import {
+  getAverageEntryPrice,
+  getProfit,
+  updateUserAssets,
+} from "../controllers/api";
 
 const router = express.Router();
 
 router
   .get(ROUTE.GET_AVERAGE_ENTRY_PRICE, getAverageEntryPrice)
-  .get(ROUTE.GET_PROFIT, getProfit);
+  .get(ROUTE.GET_PROFIT, getProfit)
+
+  .post(ROUTE.UPDATE_USER_ASSETS, updateUserAssets);
 
 export { router as api, ROUTE };
